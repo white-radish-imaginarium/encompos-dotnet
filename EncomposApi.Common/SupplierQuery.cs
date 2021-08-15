65abf6e5552ac4545e12dcc14c2b4228ad089dc2
Peter Radocchia <radocchia@gmail.com>
2021-08-15 16:49:38 -0400

    Adds suppliers query.

---
+++ EncomposApi.Common/SupplierQuery.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace EncomposApi
+{
+    public record SupplierQuery
+    {
+    }
+
+    public class SupplierQueryValidator : AbstractValidator<SupplierQuery>
+    {
+        public SupplierQueryValidator()
+        {
+        }
+    }
+}
