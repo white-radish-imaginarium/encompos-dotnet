d2bf55b057c5f237f5817db2d57b4863e941dc6a
Peter Radocchia <radocchia@gmail.com>
2021-08-22 21:07:28 -0400

    Adds query for next available product code.

---
+++ EncomposApi.Common/NextAvailableProductCodeQuery.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using FluentValidation;
+
+namespace EncomposApi
+{
+    public record NextAvailableProductCodeQuery
+    {
+        /// <summary>
+        /// Nests subdepartments.
+        /// </summary>
+        public string Prefix { get; init; }
+
+        /// <summary>
+        /// Include inventory defaults.
+        /// </summary>
+        public int Length { get; init; }
+    }
+
+    public class NextAvailableProductCodeQueryValidator : AbstractValidator<NextAvailableProductCodeQuery>
+    {
+        public NextAvailableProductCodeQueryValidator()
+        {
+            RuleFor(p => p.Prefix)
+                .Must(v => v.All(char.IsLetterOrDigit))
+                .When(v => !string.IsNullOrEmpty(v.Prefix))
+                .WithMessage("Prefix must be alphanumeric");
+
+            RuleFor(p => p.Length).LessThanOrEqualTo(15);
+            RuleFor(p => p.Length)
+                .Must((model, length) => length > model.Prefix.Length)
+                .When(v => v.Prefix != null)
+                .WithMessage("Length must be longer than the prefix.");
+        }
+    }
+}
