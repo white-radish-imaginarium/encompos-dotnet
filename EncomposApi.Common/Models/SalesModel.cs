8f01c4f33f727fde879fcacf2489f72df22f96db
Peter Radocchia <radocchia@gmail.com>
2021-09-22 21:46:37 -0400

    Shortened the names in SalesModel.

--- EncomposApi.Common/Models/SalesModel.cs
+++ EncomposApi.Common/Models/SalesModel.cs
@@ -6,9 +6,9 @@ namespace EncomposApi.Models
     public class SalesModel
     {
         public Optional<string> ProductCode { get; set; }
-        public Optional<DateTimeOffset> DateSold { get; set; }
+        public Optional<DateTimeOffset> Date { get; set; }
         public decimal Qty { get; set; }
-        public decimal TotalCost { get; set; }
-        public decimal TotalRetail { get; set; }
+        public decimal Cost { get; set; }
+        public decimal Retail { get; set; }
     }
 }
