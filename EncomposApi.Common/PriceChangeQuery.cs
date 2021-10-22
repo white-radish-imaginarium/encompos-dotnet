beb47101da3e14a542b9835eaab356fde58d4c3b
Peter Radocchia <radocchia@gmail.com>
2021-10-22 14:20:55 -0400

    Adds beforeNow and beforeId to PriceChangeQuery

--- EncomposApi.Common/PriceChangeQuery.cs
+++ EncomposApi.Common/PriceChangeQuery.cs
@@ -1,12 +1,20 @@
 ﻿using FluentValidation;
+using Newtonsoft.Json;
 using System;
 
 namespace EncomposApi
 {
     public record PriceChangeQuery
     {
+        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public bool BeforeNow { get; init; }
+
+        public decimal? BeforeId { get; init; }
+
         public decimal? SinceId { get; init; }
+
         public DateTimeOffset? SinceDate { get; init; }
+
         public int? Limit { get; init; }
     }
 
@@ -14,8 +22,9 @@ namespace EncomposApi
     {
         public PriceChangeQueryValidator()
         {
-            RuleFor(p => p.SinceId).NotNull().When(p => p.SinceDate is null);
-            RuleFor(p => p.SinceDate).NotNull().When(p => p.SinceId is null);
+            RuleFor(p => p.BeforeId).NotNull().When(p => p.SinceDate is null && p.SinceId is null && !p.BeforeNow);
+            RuleFor(p => p.SinceId).NotNull().When(p => p.SinceDate is null && p.BeforeId is null && !p.BeforeNow);
+            RuleFor(p => p.SinceDate).NotNull().When(p => p.SinceId is null && p.BeforeId is null && !p.BeforeNow);
             RuleFor(p => p.Limit).InclusiveBetween(1, 500);
         }
     }
