9f6cbac946ba8973c8f5122fd271d57f8fda723b
Peter Radocchia <radocchia@gmail.com>
2021-12-21 23:23:52 -0500

    Adds support for image controller.

---
+++ EncomposApi.Common/ImageQuery.cs
@@ -0,0 +1,8 @@
+﻿namespace EncomposApi
+{
+    public record ImageQuery
+    {
+        public string[] PluCodes { get; init; }
+        public long[] ImageIds { get; init; }
+    }
+}
---
+++ EncomposApi.Common/Models/ImageModel.cs
@@ -0,0 +1,19 @@
+﻿
+using Newtonsoft.Json;
+
+namespace EncomposApi.Models
+{
+    public class ImageModel
+    {
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+
+        public char Type { get; set; }
+
+        public long Size { get; set; }
+
+        [JsonIgnore]
+        public byte[] Bytes { get; set; }
+    }
+}
